a7623a361f5c530ef82b0aa02faf818f41d2b0bf
perseusACE <mfahadd960@gmail.com>
2024-05-28 21:41:58 +0500

    changes

--- packages_backup.sh
+++ packages_backup.sh
@@ -1,8 +1,9 @@
 echo "Backing up all packages..."
+#echo $PWD
 #pacman packages backup
-pacman -Qqen > ~/pacman_pkgs.txt
+pacman -Qqen > pacman_pkgs.txt
 
 #aur packages backup
-pacman -Qqem > ~/aur_pkgs.txt
+pacman -Qqem > aur_pkgs.txt
 
 echo "Done..."
