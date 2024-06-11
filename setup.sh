09f43e35c32de9ceb21f84a88008515d26952437
perseusACE <mfahadd960@gmail.com>
2024-06-11 14:42:06 +0500

    updated packages

---
+++ setup.sh
@@ -0,0 +1,26 @@
+
+#restore pacman packages
+echo "installing pacman packages..."
+pacman -S --needed - < pacman_pkgs.txt
+
+#restore aur packages
+pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+echo "yay installed: "
+yay --version
+echo "installing aur packages..."
+yay -S --needed - < aur_pkgs.txt
+
+#setup firewall
+echo "setting up firewall..."
+systemctl start ufw
+systemctl enable ufw
+
+ufw default allow outgoing
+ufw default deny incoming
+
+ufw allow 22/tcp
+
+echo "done!"
